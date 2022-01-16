ebed9c4b799e99d7c7c585f7d91c776f530819fe
Martin Fučík <mar.fucik@gmail.com>
2022-01-16 14:13:53 +0100

    Sending The Platform Back

--- Source/PuzzlePlatforms/MovingPlatform.h
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -20,6 +20,10 @@ public:
     UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
     FVector TargetLocation;
 
+private:
+    FVector GlobalTargetLocation;
+    FVector GlobalStartLocation;
+
 public:
     AMovingPlatform();
 
