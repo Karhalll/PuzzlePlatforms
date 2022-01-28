baa1a4dce3d26cdb253f7ec1f332740d8070da6c
Martin Fučík <mar.fucik@gmail.com>
2022-01-28 15:13:23 +0100

    Navigating Menus In C++

--- Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
@@ -4,14 +4,19 @@
 #include "MainMenu.h"
 
 #include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
 
 bool UMainMenu::Initialize()
 {
 	const bool Success = Super::Initialize();
 	if (!Success) return false;
 
-	if (!Host) return false;
-	Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+	if (!HostButton) return false;
+	HostButton->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+	if (!JoinButton) return false;
+	JoinButton->OnClicked.AddDynamic(this, &UMainMenu::OpenJoinMenu);
+	if (!BackButton) return false;
+	BackButton->OnClicked.AddDynamic(this, &UMainMenu::OpenMainMenu);
 	
 	return true;
 }
@@ -65,4 +70,18 @@ void UMainMenu::HostServer()
 	{
 		MenuInterface->Host();
 	}
+}
+
+void UMainMenu::OpenJoinMenu()
+{
+	if (!MenuSwitcher) return;
+	if (!JoinMenu) return;
+	MenuSwitcher->SetActiveWidget(JoinMenu);
+}
+
+void UMainMenu::OpenMainMenu()
+{
+	if (!MenuSwitcher) return;
+	if (!MainMenu) return;
+	MenuSwitcher->SetActiveWidget(MainMenu);
 }
